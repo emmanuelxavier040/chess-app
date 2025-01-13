ec1d81425d6a1ec714bd2ed5037ee9b39dd0e011
Emmanuel Xavier <emmanuelxavier040@gmail.com>
2025-01-13 22:36:11 +0100

    Hitting piece check added for Rook and Queen

--- chess-server/src/main/java/app/chess/game/board/Queen.java
+++ chess-server/src/main/java/app/chess/game/board/Queen.java
@@ -10,6 +10,16 @@ public class Queen extends Piece {
 
     @Override
     public void move(ChessBoard board, int[] from , int[] to) throws InvalidMoveException {
+
+        int from_rank = from[0];
+        int from_file = from[1];
+        int to_rank = to[0];
+        int to_file = to[1];
+
+        if(isHittingAPiece(board, from_rank, from_file, to_rank, to_file)) {
+            throw new InvalidMoveException("Cannot move a Queen through non-empty squares.");
+        }
+
         Square fromSquare = board.getSquare(from[0], from[1]);
         Square toSquare =  board.getSquare(to[0], to[1]);
         toSquare.setPiece(fromSquare.getPiece());
